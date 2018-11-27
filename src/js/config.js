518c76ab598a052b5f86ab079bc44aba868ccace
kimbah <kim8hughes@iinet.net.au>
2018-11-27 11:36:03 +0800

    Added getRecipe and calcTime/Servings functions

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = 'bd3c455da920d142f8ea20e15dde31bd';
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { key, proxy } from '../config';
 
 export default class Search {
     constructor (query) {
@@ -6,8 +7,6 @@ export default class Search {
     }
 
     async getResults () {
-        const proxy = 'https://cors-anywhere.herokuapp.com/';
-        const key = 'bd3c455da920d142f8ea20e15dde31bd';
         try {
             const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${this.query}`);
             console.log(res);
