d93a3e1f9531b82245865dee1207336da69e4542
kimbah <kim8hughes@iinet.net.au>
2018-12-01 11:09:25 +0800

    Added update Servings and Ingredients

--- src/js/index.js
+++ src/js/index.js
@@ -95,7 +95,7 @@ const controlRecipe = async () => {
 			state.recipe.calcServings();
 
 			// Render recipe
-			// console.log(state.recipe);
+			console.log(state.recipe);
 			clearLoader();
 			recipeView.renderRecipe(state.recipe);
 		} catch (err) {
@@ -105,3 +105,19 @@ const controlRecipe = async () => {
 };
 
 [ 'hashchange', 'load' ].forEach((event) => window.addEventListener(event, controlRecipe));
+
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', (e) => {
+	if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+		// Decrease button is clicked
+		if (state.recipe.servings > 1) {
+			state.recipe.updateServings('dec');
+			recipeView.updateServingsIngredients(state.recipe);
+		}
+	} else if (e.target.matches('.btn-increase, .btn-increase *')) {
+		// Increase button is clicked
+		state.recipe.updateServings('inc');
+		recipeView.updateServingsIngredients(state.recipe);
+	}
+	console.log(state.recipe);
+});
